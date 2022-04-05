005434fde3114e0e7990b26e4f41bccfab590a3c
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-05 15:49:03 +0200

    Vjezba 11

---
+++ ex11/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function functionElse(num) {
+  if (num <= 5) {
+    return "5 or smaller";
+  } else if (num >= 5) {
+    return "Bigger than 5";
+  }
+}
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+
+// Only change code above this line
+module.exports = functionElse;
