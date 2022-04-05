764c2de5bf83e3215e214f98d94d61114034cf70
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-05 18:58:09 +0200

    Dodata vjezba 16 i prepravljena vjezba 11

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,10 +1,12 @@
 // Only change code below this line
 function functionElse(num) {
+  var result;
   if (num <= 5) {
-    return "5 or smaller";
+    result = "5 or smaller";
   } else if (num >= 5) {
-    return "Bigger than 5";
+    result = "Bigger than 5";
   }
+  return result;
 }
 console.log(functionElse(4));
 console.log(functionElse(5));
